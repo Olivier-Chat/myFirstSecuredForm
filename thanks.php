b6425d6377689a122aed91b56b3355eef7a68b7e
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-03-23 00:02:08 +0100

    added print screen

--- thanks.php
+++ thanks.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Thank you for your message</title>
 </head>
 <body>
-    <p>
+    <p class="result">
     Merci <?=$_POST['user_first_name']?> <?=$_POST['user_last_name']?>  de nous avoir contacté à propos de “<?=$_POST['email_subject']?>”.
     Un de nos conseiller vous contactera soit à l’adresse <?=$_POST['user_email']?> ou par téléphone au <?=$_POST['user_phone']?> dans les plus brefs délais pour traiter votre demande : 
     <?=$_POST['user_message']?>
